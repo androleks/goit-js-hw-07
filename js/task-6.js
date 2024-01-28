e2f6095461573cb3227700b88ed87ae158000dbe
Oleksandr <androleks@gmail.com>
2024-01-28 19:47:02 +0100

    task6-completed

--- js/task-6.js
+++ js/task-6.js
@@ -4,3 +4,31 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
+function createBoxes(amount) {
+  const boxes = document.querySelector('#boxes');
+  boxes.innerHTML = '';
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    box.style.width = `${30 + i * 10}px`;
+    box.style.height = `${30 + i * 10}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxes.append(box);
+  }
+}
+
+function destroyBoxes() {
+  const boxes = document.querySelector('#boxes');
+  boxes.innerHTML = '';
+}
+
+document.querySelector('[data-create]').addEventListener('click', () => {
+  const amount = Number(document.querySelector('input').value);
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+  } else {
+    alert('Please enter a number between 1 and 100');
+  }
+});
+
+document.querySelector('[data-destroy]').addEventListener('click', destroyBoxes);
